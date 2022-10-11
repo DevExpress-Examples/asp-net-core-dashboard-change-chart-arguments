a253fa1e4bc4d513e807adabc170c60833ecc966
Natalia Kazakova <natalia.kazakova@devexpress.com>
2022-10-11 16:32:04 +0400

    move to a separate file

---
+++ CS/NetCoreDashboardApp/wwwroot/js/changeArguments.js
@@ -0,0 +1,21 @@
+﻿function onItemCaptionToolbarUpdated(e) {
+    if (e.itemName === 'chartDashboardItem1' || e.itemName === 'chartDashboardItem2') {
+        e.options.stateItems.push({
+            text: 'Switch Arguments',
+            type: "menu",
+            menu: {
+                type: 'list',
+                items: ['OrderDate - Year', 'OrderDate - Month-Year'],
+                selectionMode: 'single',
+                itemClick: function (item) {
+                    var viewerAPI = dashboardControl.findExtension("viewer-api");
+                    var tabContainerComponentName = "tabContainerDashboardItem1";
+                    if (item === 'OrderDate - Year')
+                        viewerAPI.setSelectedTabPageIndex(tabContainerComponentName, 0);
+                    else
+                        viewerAPI.setSelectedTabPageIndex(tabContainerComponentName, 1);
+                }
+            }
+        })
+    }
+}
